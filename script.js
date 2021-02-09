d6066bdae9f7280c4e65a1fec39d26d93a235935
dimtee <dimtaiwo@gmail.com>
2021-02-09 19:24:51 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const check = document.querySelector('.check');
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+var highScore = 0;
+let score = 20;
+console.log(highScore);
+console.log(secretNumber);
+
+check.addEventListener('click', function () {
+  console.log('clicked');
+  const guessedNumber = Number(document.querySelector('.guess').value);
+
+  if (!guessedNumber) {
+    document.querySelector('.message').textContent = 'No number entered';
+  } else if (guessedNumber === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Answer';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highScore)
+      document.querySelector('.highscore').textContent = score;
+  } else if (guessedNumber !== secretNumber) {
+    if (score !== 0) {
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent =
+        guessedNumber > secretNumber ? 'Number too high !' : 'Number too low !';
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  //displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
